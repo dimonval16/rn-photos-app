c4fc766c4fc43dec60adcce0f16e60664da01eaf
Dmytro Volynskyi <dimonval16@gmail.com>
2020-08-09 22:12:33 +0300

    refactoring

--- src/components/ImgSmall.js
+++ src/components/ImgSmall.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import UserBlock from './UserBlock';
 
 const ImgSmall = ({ url, onPhotoPress, user, desc }) => {
